b15ff6f855fced00b1a2e8f91020bfb37fe686d2
luisleee <luisleee@126.com>
2019-09-01 16:34:48 +0800

    v0.1.7

--- files.js
+++ files.js
@@ -1,39 +1,45 @@
-var fs=require("fs");
-var path=require("path");
+var fs = require("fs");
+var path = require("path");
 
-module.exports.cpdir=function(from,to){
-    var paths=fs.readdirSync(from);
-    paths.forEach(function(path){
-        var _from=from+'/'+path;
-        var _to=to+'/'+path;
-        var readable;
-        var writable;
-        var st=fs.statSync(_from);
-        if(st.isFile()){
-            fs.copyFileSync(_from,_to,"w");
-            console.log("Copying "+_from+" to "+_to);
-        }else if(st.isDirectory()){
-            cpdir(_from,_to);
+module.exports.cp = function (from, to) {
+    if (fs.existsSync(from)) {
+        fs.copyFileSync(from, to, "w");
+        console.log("Copying " + from + " to " + to);
+    }
+
+};
+
+module.exports.cpdir = function (from, to) {
+    var paths = fs.readdirSync(from);
+    paths.forEach(function (path) {
+        var _from = from + '/' + path;
+        var _to = to + '/' + path;
+        var st = fs.statSync(_from);
+        if (st.isFile()) {
+            fs.copyFileSync(_from, _to, "w");
+            console.log("Copying " + _from + " to " + _to);
+        } else if (st.isDirectory()) {
+            cpdir(_from, _to);
         }
     });
 };
 
-module.exports.ls=function(dir,suffix){
-	var match=[];
-	var files=fs.readdirSync(dir);
-	match=files.filter(function(file){
-        return path.extname(file)===suffix;
-	});
-	
-	return match;
+module.exports.ls = function (dir, suffix) {
+    var match = [];
+    var files = fs.readdirSync(dir);
+    match = files.filter(function (file) {
+        return path.extname(file) === suffix;
+    });
+
+    return match;
 };
 
-module.exports.exist=function(file){
+module.exports.exist = function (file) {
     return fs.existsSync(file);
 };
 
-module.exports.mkdir=function(dir){
-    if(this.exist(dir)){
+module.exports.mkdir = function (dir) {
+    if (this.exist(dir)) {
         console.log("DIRECTORY already exist!");
         return;
     }
@@ -41,31 +47,31 @@ module.exports.mkdir=function(dir){
 };
 
 
-module.exports.write=function(file,data,encoding){
-    fs.readFileSync(file,data,encoding);
+module.exports.write = function (file, data, encoding) {
+    return fs.writeFileSync(file, data, encoding);
 };
 
-module.exports.read=function(file,encoding){
-    fs.writeFileSync(file,encoding);
+module.exports.read = function (file, encoding) {
+    fs.readFileSync(file, encoding);
 };
 
-module.exports.stat=function(file){
+module.exports.stat = function (file) {
     return fs.statSync(file);
 };
 
-module.exports.rm=function(file){
+module.exports.rm = function (file) {
     fs.unlinkSync(file);
 };
 
-module.exports.rmdir=function(fpath){
-    var files=[];
-    if(this.exist(fpath)){
-        files=fs.readdirSync(fpath);
-        files.forEach((file,index)=>{
-            var curPath=fpath+"/"+file;
-            if(this.stat(curPath).isDirectory()){
+module.exports.rmdir = function (fpath) {
+    var files = [];
+    if (this.exist(fpath)) {
+        files = fs.readdirSync(fpath);
+        files.forEach(function (file, index) {
+            var curPath = fpath + "/" + file;
+            if (this.stat(curPath).isDirectory()) {
                 this.rmdir(curPath);
-            }else{
+            } else {
                 this.rm(curPath);
             }
         });
--- index.js
+++ index.js
@@ -1,83 +1,128 @@
 #!/usr/bin/env node
-var fs=require("fs");
-var connect=require("connect");
-var serveStatic=require("serve-static");
-var checker=require("./check");
-var md2html=require("./md2html");
-var mkindex=require("./mkindex");
-var files=require("./files");
 
-var argv=process.argv;
+var fs = require("fs");
+var connect = require("connect");
+var serveStatic = require("serve-static");
+var md2html = require("./md2html");
+var mkindex = require("./mkindex");
+var files = require("./files");
+var yaml = require("./yaml");
 
-if(argv.length<=2){
+var version = "0.1.7";
+var argv = process.argv;
+
+function usage() {
 	console.log("Usage: spfa [command]");
 	console.log("spfa i : initialize spfa");
 	console.log("spfa g : generate files");
 	console.log("spfa s : start server");
 	console.log("spfa c : clean cache");
-}else{
-	console.log("Process started.");
-	if(argv[2]==="g"||argv[2]==="generate"){
-		if(!checker.check(process.cwd())){
-			console.log("Please init first.");
-			return;
-		}
-		files.mkdir(process.cwd()+"/public");
-		files.mkdir(process.cwd()+"/public/post");
-		files.mkdir(process.cwd()+"/public/lib");
+}
 
-		var postdir=process.cwd()+"/post";
-		var list=files.ls(postdir,".md");
-		for(var i=0;i<list.length;i++){
-			var item=list[i];
-			var name=item.replace(".md","");
-			md2html.md2html(process.cwd()+"/post/"+item,process.cwd()+"/public/post/"+name+".html",name);
-		}
-		
-		mkindex.mkindex(process.cwd()+"/public/index.html",process.cwd()+"/public/post");
-		console.log("generating /public/index.html");
-		try {
-			files.cpdir(process.cwd()+"/lib",process.cwd()+"/public/lib");
-		} catch (error) {
-			console.error(error.toString());
-		}
-		
-		console.log("finished!");
-		
-	}else if(argv[2]==="s"||argv[2]==="server"){
-		if(!checker.check(process.cwd())){
-			console.log("Please init first.");
-			return;
-		}
-		var app=connect(); 
-		app.use("/",serveStatic(process.cwd()+"/public"));
-		var server=app.listen(3000);
-		console.log("Server is running on http://localhost:3000/");
-		console.log("Press ^C to stop.");
-	}else if(argv[2]==="i"||argv[2]==="init"){
-		if(checker.check(process.cwd())){
-			console.log("Already exsist!");
-			return;
-		}
-		files.write(process.cwd()+"/SPFA.tag","SPFA-TAG","utf8");
-		
-		files.mkdir(process.cwd()+"/public");
-		files.mkdir(process.cwd()+"/public/post");
-		files.mkdir(process.cwd()+"/public/lib");
-		files.mkdir(process.cwd()+"/lib");
-		files.mkdir(process.cwd()+"/post");
-		try {
-			files.cpdir(__dirname+"/lib",process.cwd()+"/lib");
-		} catch (error) {
-			console.error(error.toString());
-		}
-		console.log("finished!");
-	}else if(argv[2]==="c"||argv[2]==="clean"){
-		files.rmdir(process.cwd()+"/public");
-		console.log("cleaning /public");
-		files.mkdir(process.cwd()+"/public");
-		files.mkdir(process.cwd()+"/public/post");
-		files.mkdir(process.cwd()+"/public/lib");
-		console.log("finished!");
+function generate() {
+	if (!files.exist(process.cwd() + "/SPFA.tag")) {
+		console.log("Please init first.");
+		return;
 	}
+	var config = yaml.read(process.cwd() + "/config.yaml");
+	console.log(config);
+	var codeTheme;
+	try {
+		codeTheme = (config.codeTheme) ? (config.codeTheme) : "vs2015";
+	} catch (error) {
+		console.log("Detected error in config.yaml");
+		console.log("Please check the config.yaml again");
+		console.log("Use default code theme vs2015");
+		codeTheme = "vs2015";
+	}
+
+
+	files.mkdir(process.cwd() + "/public");
+	files.mkdir(process.cwd() + "/public/post");
+	files.mkdir(process.cwd() + "/public/lib");
+
+	var postdir = process.cwd() + "/post";
+	var list = files.ls(postdir, ".md");
+	for (var i = 0; i < list.length; i++) {
+		var item = list[i];
+		var name = item.replace(".md", "");
+		md2html.md2html(
+			process.cwd() + "/post/" + item,
+			process.cwd() + "/public/post/" + name + ".html",
+			name, codeTheme);
+	}
+
+	mkindex.mkindex(
+		process.cwd() + "/public/index.html", process.cwd() + "/public/post");
+	console.log("generating /public/index.html");
+	try {
+		files.cpdir(process.cwd() + "/lib", process.cwd() + "/public/lib");
+	} catch (error) {
+		console.error(error.toString());
+	}
+
+	console.log("finished!");
+}
+
+function server() {
+	if (!files.exist(process.cwd() + "/SPFA.tag")) {
+		console.log("Please init first.");
+		return;
+	}
+	var app = connect();
+	app.use("/", serveStatic(process.cwd() + "/public"));
+	var server = app.listen(3000);
+	console.log("Server is running on http://localhost:3000/");
+	console.log("Press ^C to stop.");
+}
+
+function init() {
+	if (files.exist(process.cwd() + "/SPFA.tag")) {
+		console.log("Repository already exsist!");
+		return;
+	}
+	files.write(process.cwd() + "/SPFA.tag", "SPFA v" + version, "utf8");
+	try {
+		files.cp(__dirname + "/config/config.yaml", process.cwd() + "/config.yaml");
+	} catch (error) {
+		console.error(error.toString());
+	}
+
+
+	files.mkdir(process.cwd() + "/public");
+	files.mkdir(process.cwd() + "/public/post");
+	files.mkdir(process.cwd() + "/public/lib");
+	files.mkdir(process.cwd() + "/lib");
+	files.mkdir(process.cwd() + "/post");
+	try {
+		files.cpdir(__dirname + "/lib", process.cwd() + "/lib");
+	} catch (error) {
+		console.error(error.toString());
+	}
+	console.log("finished!");
+}
+
+function clean() {
+	files.rmdir(process.cwd() + "/public");
+	console.log("cleaning /public");
+	files.mkdir(process.cwd() + "/public");
+	files.mkdir(process.cwd() + "/public/post");
+	files.mkdir(process.cwd() + "/public/lib");
+	console.log("finished!");
+}
+
+if (argv.length <= 2) {
+	usage();
+	return;
+}
+
+console.log("Process started.");
+if (argv[2] === "g" || argv[2] === "generate") {
+	generate();
+} else if (argv[2] === "s" || argv[2] === "server") {
+	server();
+} else if (argv[2] === "i" || argv[2] === "init") {
+	init();
+} else if (argv[2] === "c" || argv[2] === "clean") {
+	clean();
 }
